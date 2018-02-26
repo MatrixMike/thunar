201fdfdb2d01d55df9cc8ef1f010a07b153384cb
Mike J Hewitt <mikeh@electroteach.com>
2018-02-27 05:55:50 +1100

    build hints

---
+++ tips.sh
@@ -0,0 +1,11 @@
+# Mon Feb 26 22:50:50 AEDT 2018 
+
+# exo from https://bluesabre.org/2018/02/14/exo-0-12-0-stable-release/
+
+pkg-config    --list-all | grep gtk
+
+pkg-config gtk+-3.0   --libs
+
+pkg-config gtk+-3.0   --modversion
+# ./configure
+
